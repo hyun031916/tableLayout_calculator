322fc9e52384b06e12d92beca5cef8c1189344ec
ksh <hyun031916@gmail.com>
2020-09-09 02:24:57 +0900

    #4 배열의 각 버튼의 클릭 이벤트 리스너 만들기

--- app/src/main/java/com/cookandroid/tablelayout_calculator/MainActivity.java
+++ app/src/main/java/com/cookandroid/tablelayout_calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     String num1, num2;
     Integer result;
     Button[] numButtons = new Button[10];
-    Integer[] numBrnIDs = {R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3,
+    Integer[] numBtnIDs = {R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3,
     R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9};
     int i;
     @Override
@@ -69,5 +70,30 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        for (i=0; i<numBtnIDs.length; i++){
+            numButtons[i] = (Button)findViewById(numBtnIDs[i]);
+        }
+        for (i=0; i<numBtnIDs.length; i++){
+            final int index;
+            index = 1;
+
+            numButtons[index].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    if(edit1.isFocused()==true){
+                        num1 = edit1.getText().toString()
+                                +numButtons[index].getText().toString();
+                        edit1.setText(num1);
+                    }else if (edit2.isFocused()==true){
+                        num2=edit2.getText().toString()
+                                +numButtons[index].getText().toString();
+                        edit2.setText(num2);
+                    }else{
+                        Toast.makeText(getApplicationContext(),
+                                "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
