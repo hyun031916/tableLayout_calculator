400f661695d03dde85720da8ff68dd91beaddde8
ksh <hyun031916@gmail.com>
2020-09-09 02:02:51 +0900

    #2 전역변수 선언

--- app/src/main/java/com/cookandroid/tablelayout_calculator/MainActivity.java
+++ app/src/main/java/com/cookandroid/tablelayout_calculator/MainActivity.java
@@ -3,9 +3,20 @@ package com.cookandroid.tablelayout_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+    Button[] numButtons = new Button[10];
+    Integer[] numBrnIDs = {R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3,
+    R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9};
+    int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
